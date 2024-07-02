78cfcb5070369e6b65a2451e0eaf65791cf06733
Andrew Kang <akang275@gmail.com>
2024-07-02 15:35:24 -0400

    implemented design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 
+import DogData from "@/components/DogData";
 import GameSessionData from "@/components/GameSessionData";
 import Image from "next/image";
 import Papa from "papaparse";
@@ -9,6 +10,7 @@ export default function Home() {
     return (
         <main>
             <GameSessionData/>
+            {/* <DogData/> */}
         </main>
     );
 }
--- src/components/GameSessionData.tsx
+++ src/components/GameSessionData.tsx
@@ -27,7 +27,7 @@ export default function GameSessionData() {
             Papa.parse<GameSession>(csvString, {
               header: true,
               complete: (results) => {
-                // Assuming that you want to select the first dog 
+                // Assuming that you want to select the first dog/game session
                 // from the results and that at least one row exists
                 if (results.data.length > 0) {
                   setGameSession(results.data[0]);
@@ -44,40 +44,83 @@ export default function GameSessionData() {
 
     return (
         gameSession ? (
-            <div>
-                <h1>Game Sessions</h1>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Session ID</th>
-                            <th>GameState</th>
-                            <th>Dog ID</th>
-                            <th>Is Game Ended</th>
-                            <th>Num Dispenses</th>
-                            <th>Num Prompts</th>
-                            <th>Success Rate</th>
-                            <th>Num Missed</th>
-                            <th>Current Level</th>
-                            <th>Game Time</th>
-                            <th>Name</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr key={gameSession.SessionId}>
-                            <td>{gameSession.SessionId}</td>
-                            <td>{gameSession.GameState}</td>
-                            <td>{gameSession.DogId}</td>
-                            <td>{gameSession.IsGameEnded ? 'Ended' : 'Not Ended'}</td>
-                            <td>{gameSession.NumDispenses}</td>
-                            <td>{gameSession.NumPrompts}</td>
-                            <td>{gameSession.SuccessRate}</td>
-                            <td>{gameSession.NumMissed}</td>
-                            <td>{gameSession.CurrentLevel}</td>
-                            <td>{gameSession.GameTime}</td>
-                            <td>{gameSession.Name}</td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div className="flex flex-col justify-between h-screen p-12 bg-neutral-50">
+                <div className='flex flex-row justify-between basis-[30%]'>
+                    <div className='flex flex-row justify-around basis-[35%] bg-[#f2f2f2] text-black rounded-2xl'>
+                        <div className='flex flex-col justify-center'>
+                            pfp
+                        </div>
+                        <div className='flex flex-col justify-center items-start basis-'>
+                            <div className='flex justify-center font-semibold text-6xl'>
+                                {gameSession.Name}
+                            </div>
+                            <div className='flex justify-center text-[#858585]'>
+                                DOG ID: {gameSession.DogId}
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center basis-[15%] bg-[#f2f2f2] text-black rounded-2xl'>
+                        <div className='flex justify-center text-[#858585] text-2xl'>
+                            Level
+                        </div>
+                        <div className='flex justify-center font-semibold text-9xl'>
+                            {gameSession.CurrentLevel}
+                        </div>
+                    </div>
+                </div>
+                <div className='flex flex-row justify-between basis-[65%] '> 
+                    <div className='flex flex-col justify-between basis-[20%]'>
+                        <div className='flex flex-col justify-center basis-[45%] bg-[#f2f2f2] text-black rounded-2xl'>
+                            <div className='flex justify-center text-[#858585] text-2xl'>
+                                Game State
+                            </div>
+                            <div className='flex justify-center font-semibold text-9xl'>
+                                {gameSession.GameState}
+                            </div>
+                        </div>
+                        <div className='flex flex-col justify-around basis-[45%] bg-[#f2f2f2] text-black rounded-2xl'>
+                            <div className='flex justify-center text-[#858585] text-2xl'>
+                                Game Ended
+                            </div>
+                            <div className='flex justify-center font-semibold text-8xl'>
+                                {gameSession.IsGameEnded}
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-between basis-[75%]'>
+                        <div className='flex flex-row justify-center basis-[20%] bg-[#f2f2f2] text-black rounded-2xl'>
+                            <div className='flex flex-col justify-center font-semibold text-5xl'>
+                                Time Alotted: {gameSession.GameTime}
+                            </div> 
+                        </div>
+                        <div className='flex flex-row justify-between basis-[70%]'>
+                            <div className='flex flex-row justify-center bg-[#f2f2f2] text-black basis-[60%] rounded-2xl'>
+                                <div className='flex flex-col justify-evenly basis-[80%]'>
+                                    <div className='flex flex-row justify-between font-semibold text-4xl basis-'>
+                                        <div>Prompts: </div><div>{gameSession.NumPrompts}</div>
+                                    </div>
+                                    <div className='flex flex-row justify-between font-semibold text-4xl basis-'>
+                                        <div>Dispenses: </div><div>{gameSession.NumDispenses}</div>
+                                    </div>
+                                    <div className='flex flex-row justify-between font-semibold text-4xl basis-'>
+                                        <div>Missed: </div><div>{gameSession.NumMissed}</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='flex flex-col justify-center bg-[#f2f2f2] text-black basis-[35%] rounded-2xl'>
+                                <div className='flex justify-center font-semibold text-2xl'>
+                                    {gameSession.SuccessRate}
+                                </div>
+                                <div className='flex justify-center font-semibold text-2xl'>
+                                    Success Rate
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    
+                </div>
             </div>
             ) : (
                 <div>Loading...</div>
